4c931e709f36167ebec37c326af82b381f823552
ANUPAM ANURAG <65106355+Anupam125anurag@users.noreply.github.com>
2022-07-03 16:24:00 +0530

    Update ext.py

--- ext.py
+++ ext.py
@@ -1,5 +1,5 @@
 from zipfile import ZipFile
 
-with ZipFile('android-cts-12_r4-linux_x86-arm.zip', 'r') as zipObj:
+with ZipFile('/var/lib/jenkins/workspace/Android_Framework_Pixel_git/android-cts-12.1_r2-linux_x86-arm.zip', 'r') as zipObj:
    # Extract all the contents of zip file in current directory
    zipObj.extractall()
