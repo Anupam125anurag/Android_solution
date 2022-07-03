b0940c47185b200e20f092d5e209a734f4259f76
ANUPAM ANURAG <65106355+Anupam125anurag@users.noreply.github.com>
2022-07-03 16:21:45 +0530

    Update copy1.py

--- copy1.py
+++ copy1.py
@@ -1,6 +1,6 @@
 import shutil
 
-original = r'C:\Users\DELL\Downloads\android-cts-12_r4-linux_x86-arm.zip'
-target = r'E:\AnuTCS\android-cts-12_r4-linux_x86-arm.zip'
+original = r'/home/idm/Downloads/android-cts-12.1_r2-linux_x86-arm.zip'
+target = r'E:\AnuTCS/android-cts-12.1_r2-linux_x86-arm.zip'
 
 shutil.copyfile(original, target)
