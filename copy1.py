0ba675cdbc71cdc1554a598a8de21bab60b1822a
ANUPAM ANURAG <65106355+Anupam125anurag@users.noreply.github.com>
2022-07-03 16:23:11 +0530

    Update copy1.py

--- copy1.py
+++ copy1.py
@@ -1,6 +1,6 @@
 import shutil
 
 original = r'/home/idm/Downloads/android-cts-12.1_r2-linux_x86-arm.zip'
-target = r'E:\AnuTCS/android-cts-12.1_r2-linux_x86-arm.zip'
+target = r'/var/lib/jenkins/workspace/Android_Framework_Pixel_git/android-cts-12.1_r2-linux_x86-arm.zip'
 
 shutil.copyfile(original, target)
