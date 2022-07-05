c1bdde04835d34cf850d8a1f71d246d6d47bd548
ANUPAM ANURAG <65106355+Anupam125anurag@users.noreply.github.com>
2022-07-05 10:47:21 +0530

    Update settings_locators.py

--- locators/settings_locators.py
+++ locators/settings_locators.py
@@ -1,11 +1,13 @@
-device_build_id = "QSR1.210802.001\n"
+device_build_id = "SP2A.220305.012\n"
 appname = "Settings"
 settings_options_class_name = "android.widget.TextView"
+#settings_options_class_name2 = "android.widget.TextView"
+switch_class_name="android.widget.Switch"
 status_off = "OFF"
 status_on = "ON"
 
 #search button click
-search_button_app_tree_id = "com.android.launcher3:id/search_container_all_apps"
+search_button_app_tree_id = "com.google.android.apps.nexuslauncher:id/input"
 search_button_app_tree_class = "android.widget.EditText"
 
 #recent apps
@@ -14,13 +16,16 @@ recent_app_screen_id = "com.google.android.apps.nexuslauncher:id/snapshot"
 recent_app_screen_class = "android.view.View"
 
 #app search results
-click_on_app_id = "android:id/title"
+click_on_app_id = "com.google.android.settings.intelligence:id/breadcrumb"
+
 
 #wifi navigation
 network_option = "Network & internet"
+internet_option = "Internet"
 
 #wifi turning on/off
-wifi_click_switch_id = "com.android.settings:id/switchWidget"
+wifi_click_switch_id = "android:id/switch_widget"
+wifi_text = "Wi-Fi"
 
 #Bluetooth navigation
 connected_device_option = "Connected devices"
@@ -28,7 +33,7 @@ connection_preference_option = "Connection preferences"
 bluetooth_option = "Bluetooth"
 
 #bluetooth turning on/off
-bluetooth_switch_id = "com.android.settings:id/switch_widget"
+bluetooth_switch_id = "android:id/switch_widget"
 
 #battery saver in quick settings on/off
 battery_saver_option = "Battery Saver"
@@ -42,12 +47,13 @@ adb_command_build_id = "adb shell getprop ro.build.id"
 #adaptive brightness navigation
 display_option_text = "Display"
 display_option_id = "android:id/title"
+adaptive_option_text = "Use adaptive brightness"
+adaptive_option_id = "android:id/title"
 adaptive_brightness_option_text = "Adaptive brightness"
 adaptive_brightness_option_id = "android:id/title"
 
 #adaptive brightness turn on/off
 adaptive_brightness_switch_id = "android:id/switch_widget"
-
 
 
 
